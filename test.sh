19373938b7d158763a049028fb2d6e4921ba4865
mario21ic <Mario Inga Cahuana>
2019-03-14 15:36:19 -0500

    Ready

--- test.sh
+++ test.sh
@@ -4,7 +4,12 @@ docker build -t mario21ic/stress .
 
 echo "### Try ###"
 echo "docker run -ti mario21ic/stress bash"
-echo "docker run -ti --cpuset-cpus='0' --cpus=1 --cpu-shares=256 -m 300M --memory-swap -1 mario21ic/stress bash"
+
+echo "### Try cpu and memory ###"
+echo "docker run -ti --cpus=2 --cpu-shares=256 -m 300M --memory-swap -1 mario21ic/stress bash"
+
+echo "### Try cpusets: cpus and mems ###"
+echo "docker run -ti --cpuset-cpus='11' --cpuset-mems=0 --cpus=1 --cpu-shares=256 -m 300M --memory-swap -1 mario21ic/stress bash"
 
 echo "### Stress ###"
-echo "stress --cpu 8 --io 4 --vm 2 --vm-bytes 1024M --timeout 10s"
+echo "stress --cpu 8 --io 4 --vm 2 --vm-bytes 1024M --timeout 60s"
